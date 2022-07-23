343312ab94d931d8d362d084df28aa05712eaec0
ArmenArutiunian <armenasata@gmail.com>
2022-07-23 19:49:21 +0200

    Initial commit

---
+++ HW1a/raine.js
@@ -0,0 +1,100 @@
+let free;
+free = 'hello' ;
+console.log(free);
+document.write('hello');
+alert("hello");
+
+let a;
+ a = 'owu' ;
+console.log('owu');
+document.write('owu');
+alert("owu");
+
+let b;
+b = 'com' ;
+console.log('com');
+document.write('com');
+alert('com');
+
+let c;
+c = 'ua' ;
+console.log('ua');
+document.write('ua');
+alert('ua');
+
+// rgnlusdfksfgkushgefsf/sf//f/sf/ds/ffs//////////
+let n;
+n = 1;
+console.log(n);
+
+alert(1);
+
+let u;
+u= 10;
+console.log(10);
+
+alert(10);
+
+let m;
+m = -999;
+console.log(-999);
+
+alert(-999);
+
+let bb;
+bb= 123;
+console.log(123);
+
+alert(123);
+
+let e;
+e = 3.14;
+console.log(3.14);
+
+alert(3.14);
+
+let r;
+r = 2.7;
+console.log(2.7);
+
+alert(2.7);
+
+let j;
+j = 16;
+console.log(16);
+
+alert(16);
+
+document.write(n + ' ' + u + ' ' + m + ' ' + bb + ' ' + e + ' ' + r + ' ' + j + ' ' );
+// okdaowkdaopsddaisjdiajsdliahdfilajdfliasdf
+let stinger;
+stinger = 'true';
+console.log(stinger);
+document.write('true');
+alert('true');
+
+let lala;
+lala = 'false';
+console.log(lala);
+document.write('false');
+alert('false');
+
+let lastname;
+lastname = 'Arutiunian';
+console.log(lastname);
+
+let firstName;
+firstName = 'Armen';
+console.log('firstName');
+
+
+let middleName;
+middleName = 'Aramovich';
+console.log('middleName');
+
+let person;
+person =  firstName + ' ' + middleName + ' ' + lastname ;
+console.log(person)
+
+//  ПЕРШЕ ДЗ ПО ДЖС
+// 23,07,2022
---
+++ HW1b/raine.js
@@ -0,0 +1,18 @@
+let a = 100;
+
+let b = '100';
+
+let c = true;
+console.log(typeof (a) );
+console.log(typeof (b) );
+console.log(typeof (c) );
+
+
+let name = prompt('Please. Your name')
+
+let middlename = prompt( 'What is your middlename ?')
+
+let year = prompt( 'How old are you ?')
+console.log( name + ' ' + middlename + ' ' + year )
+
+
